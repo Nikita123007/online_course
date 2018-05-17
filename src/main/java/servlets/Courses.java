5a80afbab8299885f8d8d7ff11e40e25b03c0024
Дмитрий <dmitrii-1997@outlook.com>
2018-05-17 19:06:12 +0300

    Fix courses

--- src/main/java/servlets/Courses.java
+++ src/main/java/servlets/Courses.java
@@ -1,7 +1,6 @@
 package servlets;
 
 import constants.Pages;
-import constants.Roles.*;
 import dao.CourseDAO;
 import dao.DAOFactory;
 import hibernate.CourseEntity;
@@ -10,22 +9,16 @@ import hibernate.UserEntity;
 import request.AuthHelper;
 import request.CookieHelper;
 import response.ResponseData;
-import servlets.Utils.ServletHelper;
-import servlets.core.AbstractServlet;
+import servlets.Utils.Utils;
 import servlets.core.AbstractViewServlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static constants.Constants.Constant.CookieAuthToken;
 
@@ -50,16 +43,16 @@ public class Courses extends AbstractViewServlet<CourseEntity, CourseDAO> {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         UserEntity user = AuthHelper.GetAuthUser(CookieHelper.GetCookieValue(request, CookieAuthToken));
         Writer wr = response.getWriter();
-        String idCourse = request.getParameter("idCourse");
+        Integer idCourse = Utils.getInteger(request.getParameter("idCourse"));
 
-        if (user == null || idCourse == null || idCourse.equals("")){
+        if (user == null || idCourse == null){
             ResponseData responseData = new ResponseData("", Pages.Page.Courses);
             wr.write(responseData.toJson());
             wr.close();
             return;
         }
 
-        CourseEntity course = DAOFactory.getInstance().getCourseDAO().get(Integer.parseInt(idCourse));
+        CourseEntity course = DAOFactory.getInstance().getCourseDAO().get(idCourse);
         if (course == null || course.isSubscribed(user.getIdUser())) {
             ResponseData responseData = new ResponseData("", Pages.Page.Courses);
             wr.write(responseData.toJson());
@@ -68,7 +61,7 @@ public class Courses extends AbstractViewServlet<CourseEntity, CourseDAO> {
         }
 
         SubscriptionEntity subscription = new SubscriptionEntity();
-        subscription.setCourseByCourse(DAOFactory.getInstance().getCourseDAO().get(Integer.parseInt(idCourse)));
+        subscription.setCourseByCourse(DAOFactory.getInstance().getCourseDAO().get(idCourse));
         subscription.setUserByUser(user);
         subscription.setDate(new Timestamp(new Date().getTime()));
         user.getSubscriptionsByIdUser().add(subscription);
